9dc63ba7e17f4f7c4803973e164bfca42efb0a15
Emilsone <lawsonezekiel68@gmail.com>
2021-05-18 05:54:47 -0700

    create api blog

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <BlogApi/>
+  </div>
+</template>
+
+<script>
+import BlogApi from './components/BlogApi.vue'
+
+export default {
+  name: 'App',
+  components: {
+   BlogApi
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Rubik&display=swap');
+#app {
+ font-family: 'Rubik', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+   background: #313131;
+height: 100vh;
+font-size:18px;
+
+}
+*{
+padding:0px;
+margin: 0px;
+}
+</style>
---
+++ src/components/BlogApi.vue
@@ -0,0 +1,67 @@
+<template>
+  <section id="shopping-app">
+    <div id="app-vue">
+      <div class="users">
+        <div v-if="errored">
+          <p>
+            We're sorry, we're not able to retrieve this information at the
+            moment, please try back later
+          </p>
+        </div>
+
+        <div v-else>
+          <h4 v-if="loading">Loading...</h4>
+          <div v-for="post in posts" :key="post" class="post">
+            {{ post.title }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: "BlogApi",
+  data() {
+    return {
+      posts: null,
+      loading: false,
+      errored: false,
+    };
+  },
+  created() {
+    axios
+      .get(`http://jsonplaceholder.typicode.com/posts`)
+      .then((response) => {
+        // JSON responses are automatically parsed.
+        this.posts = response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+        this.errored = true;
+      });
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#app-vue {
+  display: flex;
+  justify-content: center;
+  font-family: 'Karla', sans-serif;
+  font-size: 20px;
+}
+
+.post {
+  width: 300px;
+  border: 1px solid black;
+  display: flex;
+  flex-direction: row;
+  padding: 20px;
+  background: #FFEEE4;
+  margin: 10px;
+}
+</style>
